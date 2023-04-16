019b779745a3e14ef401f19af2588edb018d3985
Kirill Kuts <p0limashine@gmail.com>
2023-04-16 18:47:56 +0200

    finished and deployed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,8 +10,8 @@ export const youtubeOptions = {
     method: "GET",
     url: "https://youtube-search-and-download.p.rapidapi.com/channel/about",
     headers: {
-        "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
         "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
     },
 };
 
